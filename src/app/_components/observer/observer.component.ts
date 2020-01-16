eb123312741d8003ddd061c08fac9d5a913a52d7
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-16 12:00:17 +0000

    generate observer component

---
+++ src/app/_components/observer/observer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-observer',
+  templateUrl: './observer.component.html',
+  styleUrls: ['./observer.component.sass']
+})
+export class ObserverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObserverComponent } from './_components/observer/observer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObserverComponent
   ],
   imports: [
     BrowserModule,
