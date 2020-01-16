b21f9b823ccac6ca6c83744b6f93ee860b067bd5
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-16 12:40:13 +0000

    observable example

--- src/app/_components/observer/observer.component.ts
+++ src/app/_components/observer/observer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observer',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./observer.component.sass']
 })
 export class ObserverComponent implements OnInit {
+  title: string;
+  observable: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.title = 'Observer';
   }
 
+  ngOnInit() {
+    this._initObservable();
+  }
+
+  _initObservable(): void {
+    this.observable = Observable.create((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      setTimeout(() => {
+        observer.next(4);
+        observer.complete();
+      }, 1000);
+    });
+  }
+
+  rxjsFunc() {
+    console.log('just before subscribe');
+    this.observable.subscribe({
+      next: nxt => console.log(`got value ${nxt}`),
+      error: err => console.log(`something wrong occured: ${err}`),
+      complete: () => console.log('done')
+    });
+    console.log('just after subscribe');
+  }
 }
